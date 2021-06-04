2a1fd743764db988262727ed762ad3b161a2c019
vitoriasue <vitoria.imamura@hotmail.com>
2021-06-04 03:29:05 -0300

    remove local db

--- lib/logic/monitor_db/monitor_db_bloc.dart
+++ lib/logic/monitor_db/monitor_db_bloc.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:gamesearch1/data/local/local_database.dart';
 import 'package:gamesearch1/data/webserver/remote_database.dart';
 import 'package:gamesearch1/logic/monitor_db/monitor_db_event.dart';
 import 'package:gamesearch1/logic/monitor_db/monitor_db_state.dart';
@@ -18,15 +16,6 @@ class MonitorBloc extends Bloc<MonitorEvent, MonitorState> {
 
   MonitorBloc() : super(MonitorState(userList: [], idList: [])) {
     add(AskNewList());
-    _localSubscription = DatabaseLocalServer.helper.stream.listen((response) {
-      try {
-        localUserList = response[0];
-        localIdList = response[1];
-        add(UpdateList(
-            userList: List.from(localUserList)..addAll(remoteUserList),
-            idList: List.from(localIdList)..addAll(remoteIdList)));
-      } catch (e) {}
-    });
 
     _remoteSubscription = DatabaseRemoteServer.helper.stream.listen((response) {
       try {
@@ -43,15 +32,11 @@ class MonitorBloc extends Bloc<MonitorEvent, MonitorState> {
   Stream<MonitorState> mapEventToState(MonitorEvent event) async* {
     if (event is AskNewList) {
       //alguem esta pedindo uma lista
-      var localResponse = await DatabaseLocalServer.helper.getUserList();
       var remoteResponse = await DatabaseRemoteServer.helper.getUserList();
-      remoteUserList = localResponse[0];
-      remoteIdList = localResponse[1];
       localUserList = remoteResponse[0];
       localIdList = remoteResponse[1];
       yield MonitorState(
-          userList: List.from(localUserList)..addAll(remoteUserList),
-          idList: List.from(localIdList)..addAll(remoteIdList));
+          userList: List.from(remoteUserList), idList: List.from(remoteIdList));
     } else if (event is UpdateList) {
       yield MonitorState(idList: event.idList, userList: event.userList);
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gamesearch1/logic/manage_db/manage_remote_db_bloc.dart';
-import 'package:gamesearch1/view/login.dart';
 import 'package:gamesearch1/view/loginweb.dart';
 import 'package:gamesearch1/view/user_list.dart';
-
-import 'logic/manage_db/manage_local_db_bloc.dart';
 import 'logic/monitor_db/monitor_db_bloc.dart';
 
 void main() {
@@ -20,7 +17,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _currentPage = 0;
 
-  var _pages = [TelaLogin(), TelaLogin1(), UserList()];
+  var _pages = [TelaLogin1(), UserList()];
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +27,6 @@ class _MyAppState extends State<MyApp> {
       home: MultiBlocProvider(
         providers: [
           BlocProvider(create: (_) => MonitorBloc()),
-          BlocProvider(create: (_) => ManageLocalBloc()),
           BlocProvider(create: (_) => ManageremoteBloc())
         ],
         child: Scaffold(
@@ -44,9 +40,7 @@ class _MyAppState extends State<MyApp> {
           bottomNavigationBar: BottomNavigationBar(
             items: [
               BottomNavigationBarItem(
-                  icon: Icon(Icons.vpn_key), label: "Login"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle), label: "Loginweb"),
+                  icon: Icon(Icons.account_circle), label: "Login Web"),
               BottomNavigationBarItem(
                   icon: Icon(Icons.account_circle), label: "Usuário"),
             ],
