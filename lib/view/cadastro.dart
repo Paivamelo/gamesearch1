b049d427affbfaaaf6fdfcd8c16f0ace964a941a
Potedesorvete <65935078+Potedesorvete@users.noreply.github.com>
2021-06-08 00:45:14 -0300

    colocando cadastro em um form

--- lib/view/cadastro.dart
+++ lib/view/cadastro.dart
@@ -9,8 +9,6 @@ class SignupPage extends StatefulWidget {
 
 class SignupPageState extends State<SignupPage> {
   GlobalKey<FormState> formKey = new GlobalKey<FormState>();
-  GlobalKey<FormState> formKey2 = new GlobalKey<FormState>();
-  GlobalKey<FormState> formKey3 = new GlobalKey<FormState>();
 
   final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
       GlobalKey<ScaffoldMessengerState>();
@@ -21,18 +19,7 @@ class SignupPageState extends State<SignupPage> {
   bool switchValue = false;
   bool switchValue1 = false;
   bool switchValue2 = false;
-  /*Map<String, bool> values = {
-    'Terror': false,
-    'Aventura': false,
-    'Estratégia': false,
-    'Battle Royale': false,
-    'FPS': false,
-    'Ação': false,
-    'Plataforma': false,
-    'Indie': false,
-    'Sandbox': false,
-  };*/
-  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -78,70 +65,76 @@ class SignupPageState extends State<SignupPage> {
             SizedBox(
               height: 20,
             ),
-            TextFormField(
-              // autofocus: true,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Nome",
-                labelStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 55,
-                  fontFamily: 'Gameover',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              // autofocus: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 55,
-                  fontFamily: 'Gameover',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              // autofocus: true,
-              obscureText: true,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 55,
-                  fontFamily: 'Gameover',
-                ),
-              ),
-            ),
             SizedBox(
               height: 20,
-            ),
-            Container(
-              child: Text(
-                "Selecione seus gêneros preferidos:",
-                style: TextStyle(
-                  fontFamily: 'Play',
-                  fontSize: 20,
-                  color: Colors.black87,
-                ),
-              ),
             ),
             Container(
               child: Form(
                 key: formKey,
                 child: Column(
                   children: [
+                    TextFormField(
+                      // autofocus: true,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Nome",
+                        labelStyle: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 55,
+                          fontFamily: 'Gameover',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      // autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 55,
+                          fontFamily: 'Gameover',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      // autofocus: true,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 55,
+                          fontFamily: 'Gameover',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Text(
+                        "Selecione seus gêneros preferidos:",
+                        style: TextStyle(
+                          fontFamily: 'Play',
+                          fontSize: 20,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
                     Row(
                       children: [
                         Text(
@@ -161,7 +154,7 @@ class SignupPageState extends State<SignupPage> {
                           },
                         ),
                         Text(
-                          "Terror",
+                          "Multijogador",
                           style: TextStyle(
                             fontFamily: 'Gameover',
                             fontSize: 45,
@@ -169,193 +162,186 @@ class SignupPageState extends State<SignupPage> {
                           ),
                         ),
                         Checkbox(
-                          value: checkBoxValue1,
+                          value: checkBoxValue,
                           onChanged: (bool value) {
                             setState(() {
-                              checkBoxValue1 = value;
+                              checkBoxValue = value;
                             });
                           },
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              child: Form(
-                key: formKey2,
-                child: Column(
-                  children: [
-                    Row(
+                    Column(
                       children: [
-                        Text(
-                          "Battle Royale",
-                          style: TextStyle(
-                            fontFamily: 'Gameover',
-                            fontSize: 45,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        Checkbox(
-                          value: checkBoxValue2,
-                          onChanged: (bool value) {
-                            setState(() {
-                              checkBoxValue2 = value;
-                            });
-                          },
-                        ),
-                        Text(
-                          "Indie",
-                          style: TextStyle(
-                            fontFamily: 'Gameover',
-                            fontSize: 45,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        Checkbox(
-                          value: checkBoxValue3,
-                          onChanged: (bool value) {
-                            setState(() {
-                              checkBoxValue3 = value;
-                            });
-                          },
+                        Row(
+                          children: [
+                            Text(
+                              "Battle Royale",
+                              style: TextStyle(
+                                fontFamily: 'Gameover',
+                                fontSize: 45,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            Checkbox(
+                              value: checkBoxValue2,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  checkBoxValue2 = value;
+                                });
+                              },
+                            ),
+                            Text(
+                              "Indie",
+                              style: TextStyle(
+                                fontFamily: 'Gameover',
+                                fontSize: 45,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            Checkbox(
+                              value: checkBoxValue3,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  checkBoxValue3 = value;
+                                });
+                              },
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: Text(
-                "Selecione as plataformas(pode ser alterado no futuro):",
-                style: TextStyle(
-                  fontFamily: 'Play',
-                  fontSize: 20,
-                  color: Colors.black87,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              alignment: Alignment.bottomLeft,
-              child: Form(
-                key: formKey3,
-                child: Column(
-                  children: [
-                    Text(
-                      "PS4",
-                      style: TextStyle(
-                        fontFamily: 'Gameover',
-                        fontSize: 45,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Switch(
-                        value: switchValue,
-                        onChanged: (bool value) {
-                          setState(() {
-                            switchValue = value;
-                          });
-                        }),
-                    Text(
-                      "PC",
-                      style: TextStyle(
-                        fontFamily: 'Gameover',
-                        fontSize: 45,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Switch(
-                        value: switchValue1,
-                        onChanged: (bool value) {
-                          setState(() {
-                            switchValue1 = value;
-                          });
-                        }),
-                    Text(
-                      "XBOX",
-                      style: TextStyle(
-                        fontFamily: 'Gameover',
-                        fontSize: 45,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Switch(
-                        value: switchValue2,
-                        onChanged: (bool value) {
-                          setState(() {
-                            switchValue2 = value;
-                          });
-                        }),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Colors.pink[400],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: ElevatedButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "CADASTRAR",
+                    Container(
+                      child: Text(
+                        "Selecione as plataformas(pode ser alterado no futuro):",
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontFamily: "Play",
-                          fontSize: 26,
-                          letterSpacing: 4,
+                          fontFamily: 'Play',
+                          fontSize: 20,
+                          color: Colors.black87,
                         ),
                       ),
-                    ],
-                  ),
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: Text("Cadastro Realizado!"),
-                      action: SnackBarAction(
-                        label: 'Undo',
-                        onPressed: () {
-                          // Some code to undo the change.
-                        },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        children: [
+                          Text(
+                            "PS4",
+                            style: TextStyle(
+                              fontFamily: 'Gameover',
+                              fontSize: 45,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          Switch(
+                              value: switchValue,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  switchValue = value;
+                                });
+                              }),
+                          Text(
+                            "PC",
+                            style: TextStyle(
+                              fontFamily: 'Gameover',
+                              fontSize: 45,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          Switch(
+                              value: switchValue1,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  switchValue1 = value;
+                                });
+                              }),
+                          Text(
+                            "XBOX",
+                            style: TextStyle(
+                              fontFamily: 'Gameover',
+                              fontSize: 45,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          Switch(
+                              value: switchValue2,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  switchValue2 = value;
+                                });
+                              }),
+                        ],
                       ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 60,
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                        color: Colors.pink[400],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
+                      child: SizedBox.expand(
+                        child: ElevatedButton(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "CADASTRAR",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontFamily: "Play",
+                                  fontSize: 26,
+                                  letterSpacing: 4,
+                                ),
+                              ),
+                            ],
+                          ),
+                          onPressed: () {
+                            final snackBar = SnackBar(
+                              content: Text("Cadastro Realizado!"),
+                              action: SnackBarAction(
+                                label: 'Undo',
+                                onPressed: () {
+                                  // Some code to undo the change.
+                                },
+                              ),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        //autofocus: true,
+                        child: Text(
+                          "Cancelar",
+                          style: TextStyle(
+                            fontFamily: 'Gameover',
+                            fontSize: 45,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        onPressed: () => Navigator.pop(context, false),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 40,
-              alignment: Alignment.center,
-              child: TextButton(
-                //autofocus: true,
-                child: Text(
-                  "Cancelar",
-                  style: TextStyle(
-                    fontFamily: 'Gameover',
-                    fontSize: 45,
-                    color: Colors.black87,
-                  ),
-                ),
-                onPressed: () => Navigator.pop(context, false),
               ),
             ),
           ],
