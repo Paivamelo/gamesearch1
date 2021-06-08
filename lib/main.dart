a3647bf946c20da69fc48d7b50f3b786d1ad366a
vitoriasue <vitoria.imamura@hotmail.com>
2021-06-08 01:38:58 -0300

    corrigindo arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gamesearch1/view/bottom_navigation_bar.dart';
 import 'package:gamesearch1/view/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(home: Splash()));
 }
 
